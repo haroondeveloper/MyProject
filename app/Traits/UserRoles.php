c491106cc1c609fc177d0650d27f684f7ce33de1
haroondeveloper <94953776+haroondeveloper@users.noreply.github.com>
2023-02-16 16:10:53 +0400

    trait UserRole

---
+++ app/Traits/UserRoles.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Traits;
+
+use App\Models\Role;
+
+trait UserRoles
+{
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function assignRole($role)
+    {
+        return $this->roles()->save(
+            Role::whereName($role)->firstOrFail()
+        );
+    }
+
+    public function revokeRole($role)
+    {
+        return $this->roles()->detach(
+            Role::whereName($role)->firstOrFail()
+        );
+    }
+
+    public function hasRole($role)
+    {
+        if (is_string($role)) {
+            return $this->roles->contains('name', $role);
+        }
+
+        return !! $role->intersect($this->roles)->count();
+    }
+}
